e6bc7fe2a209f7dfd272f93a0a51756f9a139ae0
matsu-chara <matsuy00@gmail.com>
2019-12-18 00:25:00 +0900

    add pattern and comment

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ function get_str() {
 
 function replace(str) {
   return str
-    .replace(/\n/g," ")
-    .replace(/e\.g\./g, "eg")
-    .replace(/(\d+)\.(\d+)/g, "$1_$2")
-    .replace(/\. */g,".\n\n")
-    .replace(/\? */g,"?\n\n");
+    .replace(/\n/g," ") // 不自然な改行を除去
+    .replace(/-\s/g, "") // ハイフネーション
+    .replace(/e\.g\./g, "eg") // e.g.
+    .replace(/i\.e\./g, "ie") // i.e.
+    .replace(/(\d+)\.(\d+)\.(\d+)/g, "$1_$2_$3") // section 3.2.1
+    .replace(/(\d+)\.(\d+)/g, "$1_$2") // section 3.2
+    .replace(/\. */g,".\n\n") // 文末で２回改行
+    .replace(/\? */g,"?\n\n"); // 疑問文で２回改行
 }
 
 function write_str(str) {

fcb418c8203312c1d309e512c321bf85e566c3de
matsu-chara <matsuy00@gmail.com>
2019-12-18 13:17:22 +0900

    jsfmt

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, "Action");
+  chrome.tabs.sendMessage(tab.id, "Action");
 });
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request == "Action") {
+  if (request == "Action") {
     str = get_str();
-		str = replace(str);
+    str = replace(str);
     write_str(str);
-	}
+  }
 });
 
 function get_str() {
@@ -12,14 +12,14 @@ function get_str() {
 
 function replace(str) {
   return str
-    .replace(/\n/g," ") // 不自然な改行を除去
+    .replace(/\n/g, " ") // 不自然な改行を除去
     .replace(/-\s/g, "") // ハイフネーション
     .replace(/e\.g\./g, "eg") // e.g.
     .replace(/i\.e\./g, "ie") // i.e.
     .replace(/(\d+)\.(\d+)\.(\d+)/g, "$1_$2_$3") // section 3.2.1
     .replace(/(\d+)\.(\d+)/g, "$1_$2") // section 3.2
-    .replace(/\. */g,".\n\n") // 文末で２回改行
-    .replace(/\? */g,"?\n\n"); // 疑問文で２回改行
+    .replace(/\. */g, ".\n\n") // 文末で２回改行
+    .replace(/\? */g, "?\n\n"); // 疑問文で２回改行
 }
 
 function write_str(str) {

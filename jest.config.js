0501d428fdba799a333f7f6b5f782550e6e940cf
matsu-chara <matsuy00@gmail.com>
2020-08-15 18:43:32 +0900

    remove unused config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // testMatch: ["**/src/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
-  // transform: {
-  //   "^.+\\.(ts|tsx)$": "ts-jest",
-  // },
 };

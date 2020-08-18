02c09f49df33c0b98dcfe7dbd385d3a421f8d421
cipepser <respepic@gmail.com>
2020-08-18 18:30:50 +0900

    exclude title

--- src/domain/replacer.test.ts
+++ src/domain/replacer.test.ts
@@ -10,4 +10,13 @@ sample of text.
 Fig.2.1.0 shows the result of experiments.`;
     expect(replacer.replace(original)).toEqual(expected);
   });
+
+  test("title", () => {
+    const original = `Mr. A and Ms. B talks. Mrs.C, 
+Mstr. D, Dr.E and Prt. F are friends.`;
+    const expected = `Mr. A and Ms. B talks.
+
+Mrs.C, Mstr. D, Dr.E and Prt. F are friends.`;
+    expect(replacer.replace(original)).toEqual(expected);
+  });
 });

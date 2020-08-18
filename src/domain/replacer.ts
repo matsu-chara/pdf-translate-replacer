267089069b72363dead57c3ae3130f7e54e7f711
matsu-chara <matsuy00@gmail.com>
2020-08-18 18:50:38 +0900

    reupdate

--- src/domain/replacer.ts
+++ src/domain/replacer.ts
@@ -2,6 +2,6 @@ export const replace = (str: string): string => {
   return str
     .replace(/\n/g, " ") // remove newline
     .replace(/-\s/g, "") // remove "- " e.g. somelongwor-\nds to somelongwords
-    .replace(/(?=Mr|Ms|Mrs|Mstr|Dr|Prt)\. +/g, ".\n\n") // newline at end of sentences
+    .replace(/(?<!Mr|Ms|Mrs|Mstr|Dr|Prt)\. +/g, ".\n\n") // newline at end of sentences
     .replace(/\? */g, "?\n\n"); // newline at end of questions
 };

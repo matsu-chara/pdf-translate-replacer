40cab3054fcff89eff85c55413ed5b4d3c65d276
matsu-chara <matsuy00@gmail.com>
2021-03-25 21:07:37 +0900

    update settings to webpack5

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,38 +1,36 @@
-import { ConfigurationFactory } from "webpack";
+import { Configuration } from "webpack";
 import path from "path";
 import CopyPlugin from "copy-webpack-plugin";
 
-const config: ConfigurationFactory = () => {
-  return {
-    entry: {
-      content_scripts: path.join(__dirname, "src", "content_scripts.ts"),
-      background: path.join(__dirname, "src", "background.ts"),
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].js",
-    },
-    module: {
-      rules: [
-        {
-          test: /.ts$/,
-          use: "ts-loader",
-          exclude: "/node_modules/",
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".ts", ".js"],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: "public", to: "." }],
-        options: {
-          concurrency: 100,
-        },
-      }),
+const config: Configuration = {
+  entry: {
+    content_scripts: path.join(__dirname, "src", "content_scripts.ts"),
+    background: path.join(__dirname, "src", "background.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
     ],
-  };
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
 };
 
 export default config;
